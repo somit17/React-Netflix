a97bda41b2f3e590ba1a2afbaba90acf2beece58
Somit <somitmeshram3256@gmail.com>
2021-09-02 09:00:44 +0530

    TMDB API Added

--- src/requests.js
+++ src/requests.js
@@ -1,9 +1,8 @@
 const API_KEY = "060b5ad1f77012d348b301e500dbf1f2";
 
 const requests = {
-    fetchTrending: `
-    /trending/all/week?=${API_KEY}&language=en-US`,
-
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_network=213`
 }
 
 export default requests;
